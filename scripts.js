bbbb41cea8291db9772701a174d7c5e1231b1140
Samson Nwokike <samson.ikechukwu@softescu.com>
2020-06-13 14:21:06 +0100

    javascript added

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const container = document.querySelector(".container");
+const text = document.querySelector("#text");
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breathAnimation();
+
+function breathAnimation() {
+  text.innerHTML = "Breathe In!";
+  container.className = "container grow";
+
+  setTimeout(() => {
+    text.innerHTML = "Hold";
+    setTimeout(() => {
+      text.innerHTML = "Breathe Out";
+      container.className = "container shrink";
+    }, holdTime);
+  }, breatheTime);
+}
+
+setInterval(breathAnimation, totalTime);
